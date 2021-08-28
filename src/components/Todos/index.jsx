f0628c84bedc46afc9831b736f44c45747a7fd17
kika0415 <kika91415@gmail.com>
2021-08-28 18:42:14 +0900

    init commit

---
+++ src/components/Todos/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Todo from './Todo';
+
+const Todos = () => {
+  return (
+    <div>
+      todos
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+};
+
+export default Todos;
